0d42d4ef4f2498c4f52bb7a09ed51b028eac378c
malala <nebstartrizzle@gmail.com>
2025-01-06 23:08:57 +0300

    implement user CRUD operations with database integration

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,23 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Annotated, Optional
+from app.db.database_connection import SessionLocal, get_db
+from app.models.User import User
+from app.schema.user_schema import UserInCreate, UserInResponse
 
 userrouter = APIRouter()
+db_dependency = Annotated[SessionLocal, Depends(get_db)]
 
-@userrouter.get("/")
-def list_users():
-    return {"message": "user"}
+@userrouter.get("/", status_code=status.HTTP_200_OK)
+async def list_users(db: db_dependency): # type: ignore
+    users = db.query(User).all()
+    return users
 
-@userrouter.post("/")
-def create_user():
-    return {"message": "user"}
+@userrouter.post("/", status_code=status.HTTP_201_CREATED)
+def create_user(user: UserInCreate, db: db_dependency) -> UserInResponse: # type: ignore
+    user = User(name=user.name, email=user.email, password=user.password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
-@userrouter.get("/{user_id}/")
+@userrouter.get("/{user_id}/", status_code=status.HTTP_200_OK)
 def get_user():
     return {"message": "user"}
 
-@userrouter.put("/")
+@userrouter.put("/", status_code=status.HTTP_200_OK)
 def update_user():
     return {"message": "user"}
 
-@userrouter.delete("/")
+@userrouter.delete("/", status_code=status.HTTP_200_OK)
 def delete_user():
     return {"message": "user"}
